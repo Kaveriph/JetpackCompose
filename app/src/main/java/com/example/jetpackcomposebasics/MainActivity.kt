0a345dd42179eebf97be9c792ec608e07b9cd64b
kaveri <kaveri.kaulase@walmart.com>
2024-10-06 12:57:44 +0530

    Alignment of the texts

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
@@ -21,10 +26,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeBasicsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {}
+              Surface(
+                  modifier = Modifier.fillMaxSize(),
+                  color = Color.LightGray
+              ) {
+                  GreetingText(
+                      message = "We love you Ketaki",
+                      from = "Harsha & Kaveri",
+                      modifier = Modifier.padding(10.dp)
+                  )
+              }
             }
         }
     }
@@ -32,16 +43,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier=modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
         Text(
             text = "$message",
-            fontSize = 80.sp,
-            lineHeight = 80.sp
+            fontSize = 100.sp,
+            lineHeight = 100.sp,
+            textAlign = TextAlign.Center,
         )
         Text(
             text = "From $from",
             fontSize = 40.sp,
-            lineHeight = 40.sp
+            lineHeight = 40.sp,
+            textAlign = TextAlign.End,
+            modifier = Modifier.paddingFromBaseline(80.dp)
         )
     }
 
@@ -55,7 +72,11 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun CardPreview() {
     JetpackComposeBasicsTheme {
-        GreetingText(message = "We love you Ketaki", from = "Harsha & Kaveri")
+        GreetingText(
+            message = "We love you Ketaki",
+            from = "Harsha & Kaveri",
+            modifier = Modifier.padding(8.dp)
+            )
     }
 }
 
